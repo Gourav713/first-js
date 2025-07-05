3f01e1a95409023e10212eb36880975431412398
Gourav verma <160299664+Gourav713@users.noreply.github.com>
2025-07-05 18:31:38 +0000

    Dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,11 @@
+let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.getDate());
+// console.log(mydate.getDay());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getHours());
+// console.log(mydate.getMilliseconds());
+console.log(mydate.toDateString());
+console.log(mydate.toJSON());
+console.log(mydate);
+
