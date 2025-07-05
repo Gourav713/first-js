f2407555029a70661343ed9290bfaae7ec9d1f2b
Gourav verma <160299664+Gourav713@users.noreply.github.com>
2025-07-05 18:12:43 +0000

    Math and num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,18 @@
+// here we understand the number and maths topics of java
+// console.log(Math)
+// console.log(Math.abs(-4)) // gives absolute values convert negative value to positive value
+// console.log(Math.round(4.6)) // round off value
+// console.log(Math.ceil(12.35)) // ceilling value
+// console.log(Math.floor(15.6)) // floor value
+// console.log(Math.min(1,5,7,12,1,7,5,1,5,7,1,5,4,8,9,7,))
+// console.log(Math.max(1,5,7,12,1,7,5,1,5,7,1,5,4,8,9,7,))
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor((Math.random()*10)+1));
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+
+
