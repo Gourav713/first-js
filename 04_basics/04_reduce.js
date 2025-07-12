7a3283dec4ce275a17b8689133bba33676b36bdb
Gourav verma <160299664+Gourav713@users.noreply.github.com>
2025-07-12 18:53:36 +0000

    Reduce done

---
+++ 04_basics/04_reduce.js
@@ -0,0 +1,16 @@
+// The reduce() method is an iterative method. It runs a "reducer" callback function over all elements in the array, in ascending-index order, and accumulates them into a single value.
+// const array1 = [1, 2, 3, 4];
+
+// // 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue,
+// );
+
+// console.log(sumWithInitial);
+// // Expected output: 10
+// .......................Shortcut method.......................
+const arr=[1,2,3,4,5,6]
+const sumAllValue = arr.reduce((acc , curr)=>acc+curr,0)
+console.log(sumAllValue);
